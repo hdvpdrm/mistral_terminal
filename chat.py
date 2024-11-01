d59e941c2843bfaf9b2f019b123acc7f9b3bec4d
Hdvpdrm <hardcoderyan@gmail.com>
2024-11-01 20:51:40 +0300

    class provides ability for interactive mode

---
+++ chat.py
@@ -0,0 +1,39 @@
+from mistral import Mistral
+from colors import color
+import os
+import time
+import signal
+import readline
+
+def catch_interruption_c(signal, frame):
+    print("\n work is done. Hope it was helpful!")
+    exit(0)
+
+class Chat:
+    def __init__(self,key:str,streaming:bool):
+        self.m = Mistral(key)
+        self.stream = streaming
+    
+    def __is_input_ok(self,s):
+        check = False
+        for ch in s:
+            if ch.isalpha() or ch.isdigit():
+                check = True
+        return check
+    
+    def __run_chat(self):
+        print(color.blue+"[you]:"+color.end,end="")
+
+        s = "".join(list(iter(input, '')))
+        if self.__is_input_ok(s):           
+            print(color.yellow+"[robot]:"+color.end+"\n")
+            self.m.run(s,self.stream)
+
+            
+    def run(self):
+        signal.signal(signal.SIGINT, catch_interruption_c)      
+        os.system("clear||cls")
+        while True:
+            self.__run_chat()
+            
+        signal.pause()
