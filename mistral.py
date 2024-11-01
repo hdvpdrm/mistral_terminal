a3cb05c6a71d369f76383a568f458c5e6ad9ddae
Hdvpdrm <hardcoderyan@gmail.com>
2024-11-01 20:49:15 +0300

    added ability to return response in streaming mode

--- mistral.py
+++ mistral.py
@@ -7,7 +7,7 @@ class Mistral:
         self.client = Brain(api_key=os.environ["MISTRAL_API_KEY"])
         self.model = "mistral-large-latest"
         
-    def ask(self,prompt:str) -> str:
+    def _ask(self,prompt:str) -> None:
         '''send provided prompt to Mistral AI and returns its request'''
         chat_response = self.client.chat.complete(
             model= self.model,
@@ -18,4 +18,30 @@ class Mistral:
                 },
             ]
         )
-        return chat_response.choices[0].message.content
+        print(chat_response.choices[0].message.content)
+
+    def _ask_in_stream(self,prompt:str) -> None:
+        res = self.client.chat.stream(model="mistral-small-latest", messages=[
+            {
+                "content": prompt,
+                "role": "user",
+            },
+        ])
+        length_counter = 0
+        if res is not None:
+            for event in res:
+                # handle event
+                data = event.data.choices[0]
+                content = data.delta.content
+                print(content,end="")
+                length_counter+=len(content)
+                if length_counter > 200:
+                    print("")
+                    length_counter = 0
+        print("")
+
+    def run(self,prompt:str,streaming:bool) -> None:
+        if streaming:
+            self._ask_in_stream(prompt)
+        else:
+            self._ask(prompt)
