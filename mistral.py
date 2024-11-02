8214b96238b723987027912bac68c6c25ac108fb
Hdvpdrm <hardcoderyan@gmail.com>
2024-11-02 11:21:05 +0300

    save history

--- mistral.py
+++ mistral.py
@@ -1,11 +1,13 @@
 from mistralai import Mistral as Brain
+from history import History
 import os
 
 class Mistral:
     def __init__(self,key:str):
         os.environ["MISTRAL_API_KEY"] = key
         self.client = Brain(api_key=os.environ["MISTRAL_API_KEY"])
-        self.model = "mistral-large-latest"
+        self.model = "mistral-large-latest"        
+        self.history_manager = History()
         
     def _ask(self,prompt:str) -> None:
         '''send provided prompt to Mistral AI and returns its request'''
@@ -18,7 +20,9 @@ class Mistral:
                 },
             ]
         )
-        print(chat_response.choices[0].message.content)
+        response = chat_response.choices[0].message.content
+        self.history_manager.add(prompt,response)
+        print(response)
 
     def _ask_in_stream(self,prompt:str) -> None:
         res = self.client.chat.stream(model="mistral-small-latest", messages=[
@@ -28,17 +32,20 @@ class Mistral:
             },
         ])
         length_counter = 0
+        response = []
         if res is not None:
             for event in res:
                 # handle event
                 data = event.data.choices[0]
                 content = data.delta.content
+                response.append(content)
                 print(content,end="")
                 length_counter+=len(content)
                 if length_counter > 200:
                     print("")
                     length_counter = 0
         print("")
+        self.history_manager.add(prompt,"".join(response))
 
     def run(self,prompt:str,streaming:bool) -> None:
         if streaming:
